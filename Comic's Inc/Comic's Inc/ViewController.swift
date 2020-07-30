1971c3cf87e3b966e33032319028b10f0936c497
GiovaniNunez <67969074+GiovaniNunez@users.noreply.github.com>
2020-07-30 09:52:43 -0500

    Add files via upload

---
+++ Comic's Inc/Comic's Inc/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Comic's Inc
+//
+//  Created by Giovani Nunez on 7/29/20.
+//  Copyright © 2020 Giovani Nunez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
